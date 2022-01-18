7168cb45b0a59beb88877c8de3197744a04871fa
Trung20cce <nhattrung29012002@gmail.com>
2022-01-18 11:33:31 +0700

    DiceRoll_2
    2

--- DiceRoller-1/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller-1/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.diceroller
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
 //            val toast = Toast.makeText(this, "Dice Rolled", Toast.LENGTH_SHORT).show()
 //            toast.show()
-//            val resultTextView: TextView = findViewById(R.id.textView)
+            val resultTextView: TextView = findViewById(R.id.textView)
 //            resultTextView.text = "6"
             rollDice()
         }
@@ -28,21 +28,8 @@ class MainActivity : AppCompatActivity() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
         //cập nhật kết quả sau khi diceroll lên màn hình
-//        val resultTextView: TextView = findViewById(R.id.textView)
-//        resultTextView.text = diceRoll.toString()
-        val diceImage: ImageView = findViewById(R.id.imageView)
-//        diceImage.setImageResource(R.drawable.dice_2)
-        when (diceRoll) {
-            1 -> diceImage.setImageResource(R.drawable.dice_1)
-            2 -> diceImage.setImageResource(R.drawable.dice_2)
-            3 -> diceImage.setImageResource(R.drawable.dice_3)
-            4 -> diceImage.setImageResource(R.drawable.dice_4)
-            5 -> diceImage.setImageResource(R.drawable.dice_5)
-            6 -> diceImage.setImageResource(R.drawable.dice_6)
-
-        }
-
-
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
     }
 
 }
